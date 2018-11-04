846947d99005eb74a55fdf7c1c508a384d2e54c4
Luca Rickler <luca.rickler@gmail.com>
2018-11-04 15:32:27 +0100

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,7 @@ void processInput(Window *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-struct transfMats {
-  glm::mat4 view;
-  glm::mat4 projection;
-};
-
-int main()
-{
+int main() {
   Window* window = new Window(SCR_WIDTH, SCR_HEIGHT);
   try {
     window->Open();
@@ -69,22 +63,6 @@ int main()
   mod.SetMaterial(drawMat);
   mod.SetMesh(m1);
 
-  // glm::vec3 cameraTarget(0.0f, 0.0f, 0.0f);
-  // glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
-  // glm::vec3 up(0.0f, 1.0f, 0.0f);
-
-  // transfMats *trasnfs = new transfMats();
-
-  // trasnfs->view = glm::mat4(1);
-  // trasnfs->view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), 
-  // 		   glm::vec3(0.0f, 0.0f, 0.0f), 
-  // 		   glm::vec3(0.0f, 1.0f, 0.0f));
-  // trasnfs->projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-  // glm::mat4 model(1);
-  
-  // UniformBuffer<transfMats> ubo(trasnfs, 0);
-  // ubo.SetSubData<glm::mat4>(&trasnfs->projection, sizeof(glm::mat4));
-
   Camera camera(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
     // uncomment this call to draw in wireframe polygons.
@@ -104,7 +82,6 @@ int main()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     auto m2 = glm::rotate(matrix, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
-    //auto transform = projection * view;
     mod.SetMatrix(m2);
     mod.Draw();
     
