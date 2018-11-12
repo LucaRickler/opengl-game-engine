a91563a446d3ac8f64b2e87dbaba3ee0afb5c9d7
Luca Rickler <luca.rickler@gmail.com>
2018-11-12 22:52:12 +0100

    Expanded Utils::GetTypeId

--- include/utils/utils.hpp
+++ include/utils/utils.hpp
@@ -8,6 +8,11 @@ namespace Utils {
   TypeId GetTypeId() {
     return (TypeId)typeid(T).hash_code();
   }
+
+  template <class T>
+  TypeId GetTypeId(const T& t) {
+    return (TypeId)typeid(t).hash_code();
+  }
 }
 
 #endif
